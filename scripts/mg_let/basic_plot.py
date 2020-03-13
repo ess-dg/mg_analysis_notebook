aa815774250dc70d7af2ed88aeb60fce5ad2b51a
Alexander Backis <alexander.backis@gmail.com>
2020-03-13 07:47:21 +0100

    Removed duplicated function

--- scripts/mg_let/basic_plot.py
+++ scripts/mg_let/basic_plot.py
@@ -108,43 +108,6 @@ def clusters_phs_plot(clusters, bus, vmin, vmax):
 
 
 # =============================================================================
-#                                  ToF
-# =============================================================================
-
-
-def tof_histogram(df, number_bins, title, label=None, norm=1, interval=None,
-                  color=None):
-    """
-    Histograms the ToF values in the clustered data.
-
-    Args:
-        df (DataFrame): Clustered events
-        number_bins (int): The number of bins to histogram the data into
-
-    Yields:
-        Figure containing the ToF histogram, this can then be used in overlay
-        or in sub plots.
-    """
-    # Declare parameters
-    time_offset = (0.6e-3) * 1e6
-    period_time = (1/14) * 1e6
-    weights = np.ones(df.shape[0])*norm
-    # Prepare figure
-    #plt.title('ToF - %s' % title)
-    plt.xlabel('tof ($\mu$s)')
-    plt.ylabel('Counts')
-    #plt.yscale('log')
-    plt.grid(True, which='major', linestyle='--', zorder=0)
-    plt.grid(True, which='minor', linestyle='--', zorder=0)
-    # Histogram data
-    hist, bin_edges, *_ = plt.hist((df.tof * 62.5e-9 * 1e6 + time_offset) % period_time,
-                                   bins=number_bins, zorder=4, histtype='step',
-                                   label=label, weights=weights, range=interval,
-                                   color=color)
-    bin_centers = 0.5 * (bin_edges[1:] + bin_edges[:-1])
-    #return hist, bin_centers
-
-# =============================================================================
 #                          Coincidence Histogram (2D)
 # =============================================================================
 
